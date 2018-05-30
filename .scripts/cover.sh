23f3b4e27d4145370662319bbd609c1c688be447
Dmytro Lahoza <dlahoza@gdfsi.com>
2018-05-30 19:08:46 +0300

    Travis and Coveralls

--- .scripts/cover.sh
+++ .scripts/cover.sh
@@ -3,12 +3,11 @@
 
 rm -rf ./cov
 mkdir cov
-go test -v -race -covermode=atomic -coverprofile=./cov/nats.out
-go test -v -race -covermode=atomic -coverprofile=./cov/test.out -coverpkg=github.com/DLag/cachery
-go test -v -race -covermode=atomic -coverprofile=./cov/builtin.out -coverpkg=github.com/DLag/cachery/drivers/inmemory
-go test -v -race -covermode=atomic -coverprofile=./cov/builtin.out -coverpkg=github.com/DLag/cachery/drivers/inmemory_nats
-go test -v -race -covermode=atomic -coverprofile=./cov/builtin.out -coverpkg=github.com/DLag/cachery/drivers/mock
-go test -v -race -covermode=atomic -coverprofile=./cov/builtin.out -coverpkg=github.com/DLag/cachery/drivers/redis
+go test -v -race -covermode=atomic -coverprofile=./cov/cover.out
+go test -v -race -covermode=atomic -coverprofile=./cov/inmemory.out -coverpkg=github.com/DLag/cachery/drivers/inmemory
+go test -v -race -covermode=atomic -coverprofile=./cov/inmemory_nats.out -coverpkg=github.com/DLag/cachery/drivers/inmemory_nats
+go test -v -race -covermode=atomic -coverprofile=./cov/mock.out -coverpkg=github.com/DLag/cachery/drivers/mock
+go test -v -race -covermode=atomic -coverprofile=./cov/redis.out -coverpkg=github.com/DLag/cachery/drivers/redis
 gocovmerge ./cov/*.out > acc.out
 rm -rf ./cov
 
